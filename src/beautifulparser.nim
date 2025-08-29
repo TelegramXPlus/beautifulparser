8a47fa29922de26e322c3c7727598dad803159cb
EnteryName <ferrari.lorenzo04@gmail.com>
2025-08-29 19:12:28 +0200

    refractor code and optimize loop

--- src/beautifulparser.nim
+++ src/beautifulparser.nim
@@ -7,32 +7,40 @@ export xmltree
 export options
 
 
+proc matches(child: XmlNode, options: Table[string, string]): bool =
+  ## Check if the child node matches the given options.
+  for k, v in options.pairs():
+    if not (child.attrs.hasKey(k) and child.attrs[k] == v):
+      return false
+  return true
+
+
 proc getNodes(node: XmlNode, name: string, options: Table[string, string]): seq[XmlNode] =
   ## Find all child nodes with the given name and attributes matching the options.
+  if len(options) == 0:
+    return node.findAll(name)
+
   for childr in node.findAll(name):
-    if len(options) == 0:
-      result.add(childr)
+    if childr.attrsLen == 0:
       continue
-    elif childr.attrsLen == 0:
-      continue
-    block iterOptions:
-      for k, v in options.pairs():
-        if not (childr.attrs.hasKey(k) and childr.attrs[k] == v):
-          break iterOptions
+    if matches(childr, options):
       result.add(childr)
 
 
 proc getNode(node: XmlNode, name: string, options: Table[string, string]): Option[XmlNode] =
   ## Find the first child node with the given name and attributes matching the options.
-  for childr in node.findAll(name):
-    if len(options) == 0:
-      return some(childr)
-    elif childr.attrsLen == 0:
+  let nodes = node.findAll(name)
+
+  if len(nodes) == 0:
+    return none(XmlNode)
+
+  if len(options) == 0:
+    return some(nodes[0])
+
+  for childr in nodes:
+    if childr.attrsLen == 0:
       continue
-    block iterOptions:
-      for k, v in options.pairs():
-        if not (childr.attrs.hasKey(k) and childr.attrs[k] == v):
-          break iterOptions
+    if matches(childr, options):
       return some(childr)
 
 
